785e4ae2afc815625768eede7a8f9644fa537dda
ReinaertD <reinaert.depourcq@gmail.com>
2020-07-09 11:24:47 +0200

    templates

---
+++ ReactTemplate/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Drink Coffee!</h1>
+        </div>
+    )
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
